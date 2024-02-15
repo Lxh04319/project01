4fe77cdf1c183223821fe14b84af3dc98c19bb4b
lxh11111 <lxh3162335158@qq.com>
2024-02-15 21:00:14 +0800

    短信验证

--- src/main/java/com/hmdp/service/impl/UserServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/UserServiceImpl.java
@@ -1,6 +1,7 @@
 package com.hmdp.service.impl;
 
 import cn.hutool.core.bean.BeanUtil;
+import cn.hutool.core.bean.copier.CopyOptions;
 import cn.hutool.core.util.RandomUtil;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.hmdp.dto.LoginFormDTO;
@@ -11,10 +12,19 @@ import com.hmdp.mapper.UserMapper;
 import com.hmdp.service.IUserService;
 import com.hmdp.utils.RegexUtils;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.Resource;
 import javax.servlet.http.HttpSession;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+import java.util.UUID;
+import java.util.concurrent.TimeUnit;
+
+import static com.hmdp.utils.RedisConstants.*;
 import static com.hmdp.utils.SystemConstants.USER_NICK_NAME_PREFIX;
 
 /**
@@ -26,6 +36,8 @@ import static com.hmdp.utils.SystemConstants.USER_NICK_NAME_PREFIX;
 @Slf4j
 public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements IUserService {
 
+    @Resource
+    private StringRedisTemplate stringRedisTemplate;
     @Override
     public Result sendCode(String phone, HttpSession session) {
         //校验手机号
@@ -35,8 +47,8 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements IU
         //不符合，返回
         //符合，验证码
         String code= RandomUtil.randomNumbers(6);
-        //保存验证码
-        session.setAttribute("code",code);
+        //保存验证码到redis
+        stringRedisTemplate.opsForValue().set(LOGIN_CODE_KEY+phone,code,LOGIN_CODE_TTL, TimeUnit.MINUTES);
         //发送验证码
         log.debug("发送验证码成功：{}",code);
         //返回
@@ -51,7 +63,7 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements IU
             return Result.fail("手机号格式错误");
         }
         //校验验证码
-        Object cachecode=session.getAttribute("code");
+        String cachecode=stringRedisTemplate.opsForValue().get(LOGIN_CODE_KEY+phone);
         String code=loginForm.getCode();
         if(cachecode==null||!cachecode.toString().equals(code)){
             return Result.fail("验证码错误");
@@ -63,9 +75,17 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements IU
         if(user==null){
             user=createUserWithPhone(phone);
         }
-        //保存到session
-        session.setAttribute("user", BeanUtil.copyProperties(user, UserDTO.class));
-        return null;
+        //保存到redis
+        String token=UUID.randomUUID().toString();
+        UserDTO userDTO=BeanUtil.copyProperties(user,UserDTO.class);
+        Map<String, Object> userMap=BeanUtil.beanToMap(userDTO ,new HashMap<>(),
+                CopyOptions.create()
+                        .setIgnoreNullValue(true)
+                        .setFieldValueEditor((fieldName,fieldValue)->fieldValue.toString()));
+        String tokenKey=LOGIN_USER_KEY+token;
+        stringRedisTemplate.opsForHash().putAll(tokenKey,userMap);
+        stringRedisTemplate.expire(tokenKey,LOGIN_USER_TTL,TimeUnit.MINUTES);
+        return Result.ok(token);
     }
 
     private User createUserWithPhone(String phone) {
--- src/main/java/com/hmdp/utils/LoginInterceptor.java
+++ src/main/java/com/hmdp/utils/LoginInterceptor.java
@@ -1,6 +1,9 @@
 package com.hmdp.utils;
 
+import cn.hutool.core.bean.BeanUtil;
+import cn.hutool.core.util.StrUtil;
 import com.hmdp.dto.UserDTO;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.web.servlet.HandlerInterceptor;
 
 import javax.servlet.http.HttpServletRequest;
@@ -8,26 +11,19 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import com.hmdp.entity.User;
 
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+
 public class LoginInterceptor implements HandlerInterceptor {
+
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        //获取session
-        HttpSession session=request.getSession();
-        //获取用户
-        Object user=session.getAttribute("user");
-        //判断用户是否存在
-        if(user==null){
+        //判断是否需要拦截，线程池中是否有用户
+        if(UserHolder.getUser()==null){
             response.setStatus(401);
             return false;
         }
-        //存在，保存到线程池
-        UserHolder.saveUser((UserDTO)user);
         //放行
         return true;
-    }
-
-    @Override
-    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
-        UserHolder.removeUser();
     }
 }
